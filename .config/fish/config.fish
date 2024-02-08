c92ab1c13c8c26fd3c5ce31bdbae095cfd49f6a0
Josh Elias <josh@daemontech.tools>
2024-02-08 18:45:32 -0500

    first commit from the void

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,14 @@ set fish_greeting ""
 # Handle ASDF shims
 source /opt/asdf-vm/asdf.fish
 
+# Start SSH Agent
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
